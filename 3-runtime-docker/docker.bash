c80394a0e0630821c5c68c4c39a41d0a517dec55
Ralf S. Engelschall <rse@engelschall.com>
2019-12-11 20:52:28 +0100

    upgrade and cleanup

--- 3-runtime-docker/docker.bash
+++ 3-runtime-docker/docker.bash
@@ -3,6 +3,12 @@
 ##  Copyright (c) 2019 Dr. Ralf S. Engelschall <rse@engelschall.com>
 ##  Distributed under MIT license <https://spdx.org/licenses/MIT.html>
 ##
+##  Usage:
+##  | export DOCKER_SERVER=<hostname-or-address>
+##  | source docker.bash
+##  | docker ...
+##  | docker-compose ...
+##
 
 DOCKER_HOST=tcp://${DOCKER_SERVER-127.0.0.1}:2376
 export DOCKER_HOST
@@ -18,18 +24,18 @@ DOCKER_CERT_PATH=$DOCKER_LOCAL/etc
 export DOCKER_CERT_PATH
 
 docker_fetch_cli () {
-    local docker_version=19.03.2
-    local compose_version=1.24.1
+    local docker_version=19.03.5
+    local compose_version=1.25.0
 
     echo "++ fetching docker(1) CLI from https://download.docker.com"
-    curl -sSkL --progress $(printf "%s%s" \
+    curl -sSkL $(printf "%s%s" \
         https://download.docker.com/linux/static/stable/x86_64/ \
         docker-${docker_version}.tgz) | \
         tar -z -x -f- --strip-components=1 -C $DOCKER_LOCAL/bin docker/docker
     chmod 755 $DOCKER_LOCAL/bin/docker
 
     echo "++ fetching docker-compose(1) CLI from https://github.com/docker/compose"
-    curl -sSkL --progress $(printf "%s%s" \
+    curl -sSkL $(printf "%s%s" \
         https://github.com/docker/compose/releases/download/${compose_version}/ \
         docker-compose-Linux-x86_64) -o $DOCKER_LOCAL/bin/docker-compose
     chmod 755 $DOCKER_LOCAL/bin/docker-compose
