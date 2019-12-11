6cecfcdd7db1ec174c3328864342f2405b236edf
Ralf S. Engelschall <rse@engelschall.com>
2019-12-12 00:34:45 +0100

    remove obsolete file

--- 3-runtime-docker/docker2.bash
+++
@@ -1,8 +0,0 @@
-
-export DOCKER_HOST=tcp://demo.xt.msg.team:443/ase-docker/
-export DOCKER_TLS=1
-export DOCKER_CONFIG=$(pwd)/docker.d
-
-#echo -n "admin:admin" | openssl base64 -e
-#docker run --rm -it xmartlabs/htpasswd -B -b admin admin
-
